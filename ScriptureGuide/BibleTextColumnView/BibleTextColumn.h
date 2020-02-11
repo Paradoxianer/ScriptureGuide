2f08b0514782610d0f6515661b84edc46858fc5c
Paradoxon <two4god@gmail.com>
2020-02-11 19:44:26 +0100

    added more Definitions for the BibeleTextColumn

--- ScriptureGuide/BibleTextColumnView/BibleTextColumn.h
+++ ScriptureGuide/BibleTextColumnView/BibleTextColumn.h
@@ -27,7 +27,26 @@ public:
 						BibleTextColumn(const char *moduleName, 
 										float width, float minWidth,
 										float maxWidth, alignment align);
+										
+	//+++ BColumn Methods
+										
+	virtual	void		DrawField(BField* field, BRect rect, BView* parent);
+	virtual	int			CompareFields(BField* field1, BField* field2);
+	virtual	float		GetPreferredWidth(BField* field, BView* parent) const;
+	virtual	bool		AcceptsField(const BField* field) const;
+	
+	virtual void		MouseMoved(BColumnListView* parent, BRow* row,
+							BField* field, BRect fieldRect,
+							BPoint point, uint32 buttons, int32 code);
+	virtual void		MouseDown(BColumnListView* parent, BRow* row,
+							BField* field, BRect fieldRect,
+							BPoint point, uint32 buttons);
+	virtual	void		MouseUp(BColumnListView* parent, BRow* row,
+							BField* field);
+	
+	//---- BColumn Methods
 
+	// +++ Key navigations Methods
 	const char*			GetKey();
 	const char*			GetTestament() const;	
 	const char*			GetBook() const;
@@ -46,22 +65,29 @@ public:
 	status_t			PrevBook();
 	status_t			PrevChapter();
 	status_t			PrevVerse();
+	
+	VerseKey&			KeyAt(int32 index);	
+	// --- Key navigation Methods
 
+	// +++ Text related Methods
 	void				Select(int start, int end);
-	status_t			SelectVerse(int vStart, int vEnd);
+	status_t			SelectVerse(int vStart, int vEnd);	
 	
 	const char*			VerseForSelection();
-	
+	// --- Text related Methods
+
+	// +++ Module related Methods
 	status_t			SetModule(SWModule* mod);
 	status_t			SetModule(const char* modulName);
  	SWModule*			CurrentModule(void);
+	// --- Module related Methods
 	
-	VerseKey&			KeyAt(int32 index);	
-	
+	// +++ Config Settings Methods
 	void				SetShowVerseNumbers(bool showVerseNumber);
+	bool				GetShowVersenumbers()
+							{return fShowVerseNumbers;};
 	
-	bool				GetShowVersenumbers(){return fShowVerseNumbers;};
-	
+	// --- Config Settings Method
 
 protected:
 	void				_UpdateBibleText();
