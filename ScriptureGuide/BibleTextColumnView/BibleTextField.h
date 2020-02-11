edbc1b6404f33245925efcacbac88a822c37ab90
Paradoxon <two4god@gmail.com>
2020-02-11 19:44:26 +0100

    added more Definitions for the BibeleTextColumn

--- ScriptureGuide/BibleTextColumnView/BibleTextField.h
+++ ScriptureGuide/BibleTextColumnView/BibleTextField.h
@@ -9,11 +9,20 @@
 #include <SupportDefs.h>
 
 
-class BIBLE_TEXT_FIELD{
+class BibleTextField{
 public:
+								BibleTextField(const char* key);
+
+			void				SetString(const char* string);
+			const char*			String() const;
+			void				SetClippedString(const char* string);
+			bool				HasClippedString() const;
+			const char*			ClippedString();
+			void				SetWidth(float);
+			float				Width();
 
 private:
-
+			VerseKey			key;
 };
 
 
